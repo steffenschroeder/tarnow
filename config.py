fda513a5096c1f573fa7a4c1135a0b9be3794d6b
Steffen Schröder <st.schroeder@gmail.com>
2015-11-25 19:09:27 +0100

    Added 2 new switches

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ areacode = "11111"
 executable = '/usr/local/sbin/send433'
 switches = dict(Radio=dict(unitcode=3),
                 Internet=dict(unitcode=2),
-                Nightlight=dict(unitcode=4, dontIncludeInAllRuns=True))
+                Nightlight=dict(unitcode=4, dontIncludeInAllRuns=True),
+                BigStar=dict(unitcode=5),
+                SmallStar=dict(unitcode=1))
 BOOTSTRAP_SERVE_LOCAL = True
 
 
